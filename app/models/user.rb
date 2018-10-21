c185f7a8f5ac28328d7bf2f1aa14404ce246a218
HackerRank <hackerrank@example.com>
2018-10-21 06:27:29 +0000

    Saved at Sun Oct 21 2018 12:00:51 GMT+0530 (India Standard Time)

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
   
-  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :follower_relationships, foreign_key: :followed_id, class_name: 'Follow'
   has_many :followers, through: :follower_relationships, source: :follower
 
   has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
