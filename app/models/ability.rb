e251eb79ed836c1e8f92e392bc5467fd343c119f
HackerRank <hackerrank@example.com>
2018-10-21 09:33:02 +0000

    Saved at Sun Oct 21 2018 15:06:25 GMT+0530 (India Standard Time)

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -104,10 +104,7 @@ class Ability
     can :block, User do |user_instance|
       user.blockers.pluck(:id).exclude?(user_instance.id)
     end
-
-    can :block, User do |user_instance|
-      user.blockers.pluck(:id).exclude?(user_instance.id)
-    end
+    
 
     can [:update, :blocked_list, :unblock_users, :update_password], User do |user_instance|
     end
