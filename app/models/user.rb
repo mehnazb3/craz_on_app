39d5806975d1044a258790f86714257e9ff9c46f
HackerRank <hackerrank@example.com>
2018-10-21 06:17:39 +0000

    Saved at Sun Oct 21 2018 11:51:01 GMT+0530 (India Standard Time)

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
   has_many :followers, through: :follows
-  has_many :followers, through: :follows
+  has_many :following, through: :follows
   
 
   # Associations
