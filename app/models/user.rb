fbba44712691992eb5dffef365a9d38389aed8e3
HackerRank <hackerrank@example.com>
2018-10-21 06:19:03 +0000

    Saved at Sun Oct 21 2018 11:52:25 GMT+0530 (India Standard Time)

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
+
   has_many :followers, through: :follows
-  has_many :followings, through: :follows
+  , through: :follows
   
 
   # Associations
