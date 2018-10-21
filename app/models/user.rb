40a452c9df869e7bcd73e6ca0bcc21ddfd4021dd
HackerRank <hackerrank@example.com>
2018-10-21 09:34:51 +0000

    Saved at Sun Oct 21 2018 15:08:14 GMT+0530 (India Standard Time)

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ApplicationRecord
   has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
   has_many :followed_by, through: :following_relationships, source: :following
   
-  has_many :follower_relationships, foreign_key: :followed_id, class_name: 'Follow'
+  has_many :follower_relationships, foreign_key: :followed_id, class_name: 'Bl'
   has_many :followers, through: :follower_relationships, source: :follower
 
   has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
